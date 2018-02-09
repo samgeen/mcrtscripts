6ebb95ebf960ef1c8c93e0dbdfd1c8f1190461e9
GEEN Samuel <sgeen@sapanaisvisu1.extra.cea.fr>
2018-02-09 15:57:57 +0100

    latest changes

--- IMFIC/scripts/correlatestructure.py
+++ IMFIC/scripts/correlatestructure.py
@@ -1,4 +1,4 @@
-'''
+B'''
 Correlate the structure of the cloud to the SFE
 Sam Geen, January 2018
 '''
@@ -9,10 +9,11 @@ import pymses
 from pymses.filters import CellsToPoints 
 from pymses.utils import constants as C
 
-import triaxfinder, tsfe
+import triaxfinder, tsfe, regression
 
 #parameter = "F" # Form factor a*b/c2
-parameter = "T" # (1-b^2) / (1-g^2)
+# Options: F, T, L, M, S, C
+parameter = "C" # (1-b^2) / (1-g^2)
 rhofloor = 100.0
 
 def _findtriax(snap,massweight=True,cutrho=10.0):
@@ -27,7 +28,7 @@ def _findtriax(snap,massweight=True,cutrho=10.0):
     mask = rhos > cutrho
     masses = cells["rho"]*cells.get_sizes()**3.0
     masses = masses[mask]
-    posns = cells.points[mask,:]
+    posns = cells.points[mask,:]*snap.info["unit_length"].express(C.pc)
     if not massweight:
         masses *= 0.0
         masses += 1.0
@@ -69,6 +70,19 @@ def FormParameter(a,b,c):
         beta = axes[1]/axes[2]
         gamma = axes[0]/axes[2]
         return (1.0-beta**2) / (1.0-gamma**2)
+    if parameter == "C":
+        # Basic compactness, radial size of dense parts
+        return np.sqrt(np.sum(axes**2))
+    if parameter == "L":
+        # Longest axis
+        return axes[2]
+    if parameter == "M":
+        # Middlest axis
+        return axes[1]
+    if parameter == "S":
+        # Sweet baby axis
+        return axes[0]
+
 
 def runforsim(simname):
     print "Running for simulation", simname
@@ -95,23 +109,35 @@ def run(simnames,plotname):
         tsfes.append(sfe)
         Fs.append(F)
         cols.append(col)
+        label = linestyles.Label(simname)
+        plt.scatter(F,sfe*100,s=80,marker="o",c=col,edgecolors='k',zorder=2)
+        plt.gca().annotate(r".\ "+label, (F,sfe*100),fontsize="x-small",zorder=1)
     tsfes = np.array(tsfes)
     Fs = np.array(Fs)
-    plt.scatter(Fs,tsfes*100,s=80,marker="o",c=cols,edgecolors='k')
     if parameter == "F":
         maxF = np.max([np.max(Fs),10.0])
         minF = np.min([np.min(Fs),0.1])
+        plt.xlim([minF,maxF])
         plt.xscale("log")
         plt.xlabel(r"Filamentary $\leftarrow$ $F$ (\textonehalf $t_{ff}) \rightarrow$ Disky")
     if parameter == "T":
         maxF = 1.0
         minF = 0.0
+        plt.xlim([minF,maxF])
         plt.xlabel(r"Disky $\leftarrow$ $T$ (\textonehalf $t_{ff}) \rightarrow$ Filamentary")
-    plt.xlim([minF,maxF])
+    if parameter == "C":
+        plt.xlabel("Mean radius of cloud / pc")
+    if parameter == "L":
+        plt.xlabel("Length of longest axis / pc")
+    if parameter == "M":
+        plt.xlabel("Length of middlest axis / pc")
+    if parameter == "S":
+        plt.xlabel("Length of shortest axis / pc")
     plt.ylabel("$\%$ TSFE (final)")
     plt.yscale("log")
-    plt.savefig("../plots/correlatestructure_"+parameter+"_n"+str(rhofloor).replace(".","p")+"parameter.pdf")
-
+    plotname = "../plots/correlatestructure_"+parameter+"_n"+str(rhofloor).replace(".","p")+"parameter.pdf"
+    plt.savefig(plotname)
+    regression.writecoeff(np.log10(Fs),np.log10(tsfes*100),plotname.replace(".pdf","_rxy.txt"))
     
 
 if __name__=="__main__":
