ac65e5175a6722c32dc411fa2e837eb49c3d25eb
Samuel Geen <samgeen@int2.bullx>
2019-11-04 14:40:44 +0100

    Cubes for Juan project added

---
+++ CubesForJuan/scripts/pymses_field_descrs.py
@@ -0,0 +1,32 @@
+# File descriptor in simulation
+import pymses
+from pymses.sources.ramses.output import *
+
+myrtvars = []
+ip = 0
+ir = False
+
+radbins = ["HII","HeII","HeIII"]
+if ir:
+    radbins = ["IR","Opt","HII","HeII","HeIII"]
+for p in radbins:
+    myrtvars.append(Scalar("Np"+p,ip))
+    myrtvars.append(Vector("Fp"+p,[ip+1,ip+2,ip+3]))
+    ip += 4
+
+self.amr_field_descrs_by_file = \
+    {
+    "3D": {"hydro" : [ Scalar("rho", 0), Vector("vel", [1, 2, 3]), 
+                       Vector("B-left", [4, 5, 6]), 
+                       Vector("B-right", [7, 8, 9]), 
+                       Scalar("Pnontherm",10),
+                       Scalar("P", 11), 
+                       Scalar("xHII",12), Scalar("xHeII",13), Scalar("xHeIII",14)],
+           "rt"    : myrtvars
+           }
+    }
+print "Read user field descriptors by Sam Geen"
+#fields = pymses.sources.ramses.output.RamsesOutput.amr_field_descrs_by_file
+#hs = fields["3D"]["hydro"]
+#for h in hs:
+#    print h.name
