03bbfb5b1cc4ef338b6a7dc7a7cdfe903ac5eefc
Sam Geen <samgeen@gmail.com>
2019-12-17 17:21:20 +0100

    Updated rdmfile usage in AMUN

--- scripts/rdmfile.py
+++ scripts/rdmfile.py
@@ -11,7 +11,7 @@ class RDMFile(object):
     '''
     This is a file object used to create .fits archive files for Research Data Management
     Useage:
-    rdm = RDMFile()
+    rdm = RDMFile(__file__) # ("__file__" is to get the module name to trace what generated the figure)
     rdm.AddPoints(np.random(20),np.random(20),label="XY Random Noise")
     rdm.AddArray(np.zeros((300,200)),label="Empty image")
     rdm.Write("../plots/mycoolrdmfile.fits")
@@ -19,11 +19,12 @@ class RDMFile(object):
     We could also skim pyplot objects for points, etc, but that sounds like a lot of work
     '''
 
-    def __init__(self):
+    def __init__(self,modulename):
         '''
         Constructor
         '''
         # List of items to put in the fits file
+        self._modulename = modulename
         self._itemlist = []
         self._first = True
 
@@ -52,8 +53,9 @@ class RDMFile(object):
         if ".pdf" in filename or ".eps" in filename:
             filename = filename.replace(".pdf",".fits")
         hdul = fits.HDUList(self._itemlist)
-        hdul.writeto(filename)
-    
+        hdul.writeto(filename,overwrite=True)
+
+    # NON-INTERFACE FUNCTION _AddHDU
     def _AddHDU(self,array,label=None):
         '''
         Private method that creates HDUs for the output file
@@ -62,9 +64,11 @@ class RDMFile(object):
             label = ""
         if self._first:
             hdu = fits.PrimaryHDU(array)
-            self._first = False
         else:
             hdu = fits.ImageHDU(array)
         hdr = hdu.header
         hdr["LABEL"] = label
+        if self._first:
+            hdr["MODULENAME"] = self._modulename
         self._itemlist.append(hdu)
+        self._first = False
