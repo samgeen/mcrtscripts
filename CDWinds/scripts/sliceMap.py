Fixed units of eddy speed compare

CDWinds/scripts/sliceMap.py
@@ -198,8 +198,8 @@ def _MapSlice(snap,hydro='rho',los='z',zoom=1.0,starC=False):
             slc = 1.0 / slc
         # Compare the eddy turnover speed (dxphys / curl V) to the bulk gas speed
         if "speedcompare" in hydro:
-            spd = np.sqrt(vx0**2 + vy0**2 + vz0**2) / dxphys
-            slc = dxphys * slc / spd
+            spd = np.sqrt(vx0**2 + vy0**2 + vz0**2) * dxphys
+            slc = dxphys * slc / Myrins / spd
     return slc
 
 _MapSliceHamu = Hamu.Algorithm(_MapSlice)