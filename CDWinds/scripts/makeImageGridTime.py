ad112b079dd0a3126f85181f8be40ea31391de25
Sam Geen <samgeen@gmail.com>
2022-03-11 15:55:08 +0100

    Typo

--- CDWinds/scripts/makeImageGridTime.py
+++ CDWinds/scripts/makeImageGridTime.py
@@ -636,12 +636,12 @@ if __name__=="__main__":
 
             # Single slices
             # HACK - FORCE RERUN OF ALL PLOTS
-            HamuLite.GLOBALFORCEREPLACECACHE = True
+            Hamu.GLOBALFORCEREPLACECACHE = True
             for hydro in ["vorticity1px_speedcompare"]:
                 MakeFigure([simset[0]],[timesin[-1]],name=figname+"singleslice",los=los,hydro=hydro,
                             Slice=True,wsink=True,starC=False,
                             timeL=[timeL[-1]],zoom=zoom,forcerun=True)
-            HamuLite.GLOBALFORCEREPLACECACHE = False
+            Hamu.GLOBALFORCEREPLACECACHE = False
 
             for hydro in ["vorticity1px_speedcompare","vorticity4px_timescale2","vorticity1px","vorticity2px","vorticity4px",
                           "Lcool","T","rho","xHII","xHeII","xHeIII",
